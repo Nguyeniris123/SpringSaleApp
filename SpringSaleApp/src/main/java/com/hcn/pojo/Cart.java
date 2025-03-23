c59ee0b64ed448c29e07ecbcdc1b4cbf4f1594e7
Your Name <you@example.com>
2025-03-23 16:15:00 +0700

    PTHHW th3

---
+++ SpringSaleApp/src/main/java/com/hcn/pojo/Cart.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hcn.pojo;
+
+/**
+ *
+ * @author nguyenho
+ */
+public class Cart {
+    private int id;
+    private String name;
+    private long price;
+    private int quantity;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the price
+     */
+    public long getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    /**
+     * @return the quantity
+     */
+    public int getQuantity() {
+        return quantity;
+    }
+
+    /**
+     * @param quantity the quantity to set
+     */
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
---
+++ SpringSaleApp/src/main/java/com/hcn/services/impl/CategoryServiceImpl.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hcn.services.impl;
+
+import com.hcn.pojo.Category;
+import com.hcn.repositories.CategoryRepository;
+import com.hcn.services.CategoryService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author admin
+ */
+@Service
+public class CategoryServiceImpl implements CategoryService{
+    @Autowired
+    private CategoryRepository cateRepo;
+    
+    @Override
+    public List<Category> getCates() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
